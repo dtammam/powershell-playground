5c165d0f81c56dfa84a931d2bbc37796e6d76339
Dean <dean@tamm.am>
2022-08-06 18:38:01 -0400

    Update ModuleCom.ps1

--- ModuleCom/ModuleCom.ps1
+++ ModuleCom/ModuleCom.ps1
@@ -19,16 +19,17 @@ ModuleCom.ps1
         N/A
 #>
 
-# Script-specific global variables for logging/record-keeping
+# Script-specific global variables for logging/events/exiting with declared exit codes
 $Global:ScriptName = $MyInvocation.MyCommand
 $Global:SuccessEventID = '601'
 $Global:FailureEventID = '603'
 $Global:SuccessExitCode = '0'
 $Global:FailureExitCode = '1'
 
-# Import functions from common utitilies
+# Import functions from common utilities module, removing it first in case anything is sitting in memory
 Remove-Module -Name CommonUtilities -ErrorAction SilentlyContinue
 Import-Module -Name "$($PSScriptRoot)\CommonUtilities" -Force
+# Now that it's imported, get the cmdlets out of the module
 Get-Command -Module CommonUtilities
 
 # Overarching Try block for execution
@@ -37,11 +38,13 @@ try {
     Start-Transcript -Path $Global:LogTailPath -Append
 
     # Main execution logic
+    # Notice that the Write-Log function is not defined in the script... the module has it!
     Write-Log "This script loaded a PowerShell Module manifest, is using one to write this log right now!"
     Write-Log "You can try 'Get-Help Write-Log' or 'Get-Help Write-Event if you'd like! I'm going to beep now."
     [System.Console]::Beep(500,300)
     Start-Sleep -Seconds 1.5
     Write-Log "Once I'm finished... check the Event Viewer for the 'ModuleCom' event in Application logs. Also 'C:\Program Files\_ScriptLogs'. Goodbye!"
+    # Notice that the Write-Event function is not defined in the script... the module has it too!
     Write-Event -EventId $Global:SuccessEventID -Message $($Global:EventMessage | Out-String)
 
     # Cleanup and exit script
