d67b9a715a5b2d7fe52f1264768faaed31cdb24c
Dean <dean@tamm.am>
2022-08-07 02:19:16 -0400

    Update CommonUtilities.psm1

--- ModuleCom/CommonUtilities/CommonUtilities.psm1
+++ ModuleCom/CommonUtilities/CommonUtilities.psm1
@@ -2,11 +2,12 @@
 CommonUtilities.psm1
 
     Goal:
-        Save time
-        One place to maintain useful code
-        Extend and modify funcions and all scripts that use it immediately
-        Makes caling scripts more readable
-        Portable and sharable
+        Centralized module that will:
+        - Save time
+        - One place to maintain useful code
+        - Extend and modify funcions and all scripts that use it immediately
+        - Makes caling scripts more readable
+        - Portable and sharable
 
     Audience:
         PowerShell scripters who want to leverage modules
@@ -15,13 +16,13 @@ CommonUtilities.psm1
         8/6/2022 - Original version
 #>
 
-#  Get the path to the function files...
+#  Get the path to the function files
 $FunctionPath = $PSScriptRoot + "\Function\"
 
-# Get a list of all the function file names...
+# Get a list of all the function file names
 $FunctionList = Get-ChildItem -Path $FunctionPath -Name 
 
-#  Loop over alll the files and dot source them into memory..
-foreach ($Function in $FunctionList) {
+#  Loop over all the files and dot source them into memory
+ForEach ($Function in $FunctionList) {
     . ($FunctionPath + $Function) 
 }
