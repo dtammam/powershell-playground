8c33e75ccab1272516a26ce0b976aab375f3a84f
Dean <dean@tamm.am>
2023-09-22 16:37:40 -0400

    Create RegLauncher.ps1
    New idea to try to create a launcher to open the reg at the target key.

---
+++ RegLauncher/RegLauncher.ps1
@@ -0,0 +1,7 @@
+﻿Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit
+
+Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Communications
+
+reg add 
+
+LastKey
