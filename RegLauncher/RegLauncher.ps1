63a014a8737faf5893fd22dd4f8d44052875c368
Dean <dean@tamm.am>
2023-09-22 20:42:02 -0400

    Now I've got a registry shortcut editor!
    Thanks for teaching me about this, Joe. This'll be useful.

--- RegLauncher/RegLauncher.ps1
+++ RegLauncher/RegLauncher.ps1
@@ -1,7 +1,43 @@
-﻿Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit
+﻿<#
+.SYNOPSIS
+    Sets the 'LastKey' registry key to open regedit to the desired key.
 
-Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Communications
+.DESCRIPTION
+    The Set-LastKey function updates the 'LastKey' registry key in the path 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit'. This key determines the default location that the Registry Editor (regedit) opens to. By setting this key, you can control the initial location that regedit navigates to upon opening.
 
-reg add 
+.PARAMETER DesiredKey
+    The registry path that you want regedit to open to by default.
 
-LastKey
+.EXAMPLE
+    .\RegLauncher.ps1 -DesiredKey "HKEY_CURRENT_USER\Software\Microsoft"
+
+    This command sets the 'LastKey' registry key to open regedit to the path "HKEY_CURRENT_USER\Software\Microsoft".
+
+.NOTES
+    Useful when creating shortcuts to target keys, so you can use a shortcut to quickly access the target key.
+#>
+
+# Define the parameter for the script
+param (
+    [Parameter(Mandatory = $true)]
+    [String]$DesiredKey
+)
+
+function Set-LastKey {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]$DesiredKey
+    )
+    try {
+        Write-Host "Setting 'LastKey' registry key to open regedit to the desired key..."
+        reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" /v LastKey /t REG_SZ /d $DesiredKey /f
+        Write-Host "Set the 'LastKey' registry key successfully."
+    } catch {
+        Write-Error "Failed to set the 'LastKey' registry key."
+    }
+}
+
+# Call the function using the script parameter
+Set-LastKey -DesiredKey $DesiredKey
+Start-Process -FilePath regedit.exe -ArgumentList "/m"
