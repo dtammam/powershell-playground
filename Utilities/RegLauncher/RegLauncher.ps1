7136e6a57b19445d6e33b3a427c1047231faf561
Dean <dean@tamm.am>
2024-05-15 21:04:38 -0400

    Standardized code with recent learnings
    Updated docstring, spacing and line lengths in alignment with recently learned best practices

--- Utilities/RegLauncher/RegLauncher.ps1
+++ Utilities/RegLauncher/RegLauncher.ps1
@@ -1,32 +1,35 @@
 ﻿<#
 .SYNOPSIS
     Sets the 'LastKey' registry key to open regedit to the desired key.
-
 .DESCRIPTION
-    The Set-LastKey function updates the 'LastKey' registry key in the path 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit'. This key determines the default location that the Registry Editor (regedit) opens to. By setting this key, you can control the initial location that regedit navigates to upon opening.
-
+    The Set-LastKey function updates the 'LastKey' registry key in the path
+    'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit'. 
+    This key determines the default location that regedit opens to. By setting this key, 
+    you can control the initial location that regedit navigates to upon opening.
 .PARAMETER DesiredKey
     The registry path that you want regedit to open to by default.
-
 .EXAMPLE
     .\RegLauncher.ps1 -DesiredKey "HKEY_CURRENT_USER\Software\Microsoft"
 
     This command sets the 'LastKey' registry key to open regedit to the path "HKEY_CURRENT_USER\Software\Microsoft".
+.EXAMPLE
+    C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File "C:\Repo\RegLauncher.ps1" -DesiredKey 
+    "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 
+    This Target within a Windows shortcut facilitates one opening the 32-bit Uninstall key within Regedit.
 .NOTES
     Useful when creating shortcuts to target keys, so you can use a shortcut to quickly access the target key.
 #>
 
 # Define the parameter for the script
 param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [String]$DesiredKey
 )
-
 function Set-LastKey {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory)]
         [String]$DesiredKey
     )
     try {
