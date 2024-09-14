e8138f6332fe84edf77bfe684607cd214b4e0c47
Dean <dean@tamm.am>
2024-09-14 13:06:30 -0400

    DosBox pattern solved (simply)!
    Now, I just point shortcuts to the PowerShell script. Reference the game executable and the script takes care of the rest, with me as a user just double-clicking a shortcut.

---
+++ Projects/Dosbox/Invoke-DosGame.ps1
@@ -0,0 +1,38 @@
+﻿param (
+    [Parameter(Mandatory=$false)]
+    [string]$DosboxPath = "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe",
+    [Parameter(Mandatory)]
+    [string]$GameExecutablePath
+)
+
+function Invoke-DOSGame {
+    <#
+    .SYNOPSIS
+        Launch a DOS game leveraging Dosbox programmatically.
+    .DESCRIPTION
+        Launch a DOS game by providing the full path to the game executable.
+        No need to worry about configuring the game or running manually each time with a modern shortcut!
+    .PARAM DosboxPath
+        Path to the Doxbox executable.
+        Non-mandatory parameter, the default 32-bit version path is pre-populated.
+    .PARAM GameExecutablePath
+        Full path to the game executable file.
+    .NOTES
+        DosBox is a prerequisite app that needs to be installed for this to work.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$DosboxPath,
+        [Parameter(Mandatory)]
+        [string]$GameExecutablePath
+    )
+
+    $gamePath = Split-Path $GameExecutablePath -Parent
+    $gameExecutable = Split-Path $GameExecutablePath -Leaf
+
+    Start-Process $DosboxPath -ArgumentList "-c `"mount C $gamePath`" -c `"C:`" -c `"$gameExecutable`" -c `"exit`""
+}
+
+Invoke-DosGame -DosboxPath $DosboxPath -GameExecutablePath $GameExecutablePath
